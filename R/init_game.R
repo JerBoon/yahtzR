a745ba004368f34ca3a59a5d057b4d6340104a83
Jer Boon <jer.booner@gmail.com>
2019-08-31 17:42:47 +0100

    Initialise data table, and a few bits

--- R/init_game.R
+++ R/init_game.R
@@ -3,11 +3,24 @@
 
 init_game <- function() {
 
+  t <- data.frame(
+    half=c(1,1,1,1,1,1,2,2,2,2,2,2,2),
+    section=c("x1","x2","x3","x4","x5","x6","3k","4k","fh","ss","ls","yz","ch"),
+    name=c("Aces","Twos","Threes","Fours","Fives","Sixes",
+           "3 of a kind","4 of a kind","Full house","Small straight","Long straight",
+           "Yahtzee","Chance"),
+    score=NA,
+    score.available=NA
+  )
+
   x <- list(
     dice = NA,
-    rolls = 0
+    rolls = 0,
+    table=t
   )
 
   return(x)
 
 }
+
+
--- R/yahtzR.R
+++ R/yahtzR.R
@@ -5,7 +5,11 @@
 #'
 #' Play the game of yahtzee in an R session
 #'
-#' @param work.mode Logical. If TRUE, then display the game in a manner which looks like you're working!
+#' Yahtzee is a classic dice rolling game and, heck, why not play it in R instead of doing some
+#' important machine learning or whatever.
+#'
+#' @param work.mode Logical. If TRUE, then display the game in a manner which looks a bit
+#'   more like you're actually working!
 #'
 #'   N.B. I accept no responsibility if your boss still notices!
 #'
