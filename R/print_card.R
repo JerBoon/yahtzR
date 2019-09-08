cbdbf7351143735b469782f10ec7c068c5f1ab4d
Jer Boon <jer.booner@gmail.com>
2019-09-08 19:43:20 +0100

    Don't print "NA" in totals on new card! Fixes #14

--- R/print_card.R
+++ R/print_card.R
@@ -74,9 +74,13 @@ print_card <- function(game,work.mode=F) {
 
     ## Print total summary box
 
+    na0 <- function(x) { if (is.na(x)) 0 else x }
+
     if (add.totals) {
-      cat(sprintf("|Upper total     %3d| |Game: %-13s|\n", p[p$section=="ut","score"], class(game)))
-      cat(sprintf("|Lower total    %4d| |TOTAL          %4d|\n",p[p$section=="lt","score"],p[p$section=="gt","score"]))
+      cat(sprintf("|Upper total     %3d| |Game: %-13s|\n", na0(p[p$section=="ut","score"]), class(game)))
+      cat(sprintf("|Lower total    %4d| |TOTAL          %4d|\n",
+                  na0(p[p$section=="lt","score"]),
+                  na0(p[p$section=="gt","score"])))
       cat("+-------------------+ +-------------------+\n")
     }
   }
