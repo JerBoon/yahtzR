6a71fb70aff6bc913d42e3e9c33cae99b0612ef0
Jer Boon <jer.booner@gmail.com>
2019-09-30 20:18:27 +0100

    Added lollipot charts

--- R/plot_card.R
+++ R/plot_card.R
@@ -23,8 +23,10 @@ plot_card <- function(game, work.mode) {
   # colour palette
   base_palette <- sample(c("Greens","Oranges","Accent","Dark2","Paired","Set1","Set2"),1)
 
+  random_colour <- sample(c("red","green","orange","blue","black","grey"),1)
+
   # Print a random chart
-  style <-  sample(1:3,1)
+  style <-  sample(1:5,1)
 
   # ------------------------
 
@@ -55,7 +57,7 @@ plot_card <- function(game, work.mode) {
       gg <- gg +
         ggplot2::theme(axis.text.x = ggplot2::element_text(angle=30,vjust=1,hjust=1))
   }
-  else
+  else if (style == 3)
   {
     #Vertical column plot with facets
     gg <- ggplot2::ggplot(gg.dat, ggplot2::aes(x=label,y=score.z, fill=factor(half))) +
@@ -71,6 +73,42 @@ plot_card <- function(game, work.mode) {
     if (!work.mode)
       gg <- gg +
       ggplot2::theme(axis.text.x = ggplot2::element_text(angle=30,vjust=1,hjust=1))
+  }
+  else if (style == 4)
+  {
+    #Horizontal lollipoplot
+    gg <- ggplot2::ggplot(gg.dat, ggplot2::aes(x=label,y=score, fill=factor(half), group=factor(half))) +
+      base_theme +
+      ggplot2::scale_fill_brewer(palette = base_palette) +
+      ggplot2::geom_linerange(ggplot2::aes(ymin=0,ymax=score), alpha=sample(c(0.3,0.5,1),1), colour=random_colour, size=sample(c(1,1.5,2),1)) +
+      ggplot2::geom_point(color=random_colour, size=sample(2:4,1)) +
+      ggplot2::scale_x_discrete(name="", limits=rev(gg.dat$label)) +
+      ggplot2::ylab("") +
+      ggplot2::theme(legend.position = "none") +
+      ggplot2::coord_flip()
+  }
+  else if (style == 5)
+  {
+    #Vertical  lollipop
+    gg <- ggplot2::ggplot(gg.dat, ggplot2::aes(x=label,y=score, fill=factor(half), group=factor(half))) +
+      base_theme +
+      ggplot2::scale_fill_brewer(palette = base_palette) +
+      ggplot2::geom_linerange(ggplot2::aes(ymin=0,ymax=score), alpha=sample(c(0.3,0.5,1),1), colour=random_colour, size=sample(c(1,1.5,2),1)) +
+      ggplot2::geom_point(color=random_colour, size=sample(2:6,1)) +
+      ggplot2::ylab("") +
+      ggplot2::theme(legend.position = "none")
+
+    # Occasionally reverse the y axis - because why not :)
+    if (runif(1)<0.3)
+      gg <- gg +
+        ggplot2::scale_x_discrete(name="", position="top") +
+        ggplot2::scale_y_reverse() +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle=90, hjust=0, vjust=0.5))
+    else
+      gg <- gg +
+        ggplot2::scale_x_discrete(name="") +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(angle=90, hjust=1, vjust=0.5))
+
   }
 
   # Standard titles
